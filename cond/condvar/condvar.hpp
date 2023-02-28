9c285c9d6286effa0bbea6fb5fac9a87104026af
maxim <belov.ma@phystech.edu>
2023-02-28 16:16:29 +0300

    Bump task cond/condvar

--- cond/condvar/condvar.hpp
+++ cond/condvar/condvar.hpp
@@ -1,11 +1,7 @@
 #pragma once
 
-#include <memory>
-#include <mutex>
-#include <queue>
 #include <twist/ed/stdlike/atomic.hpp>
 #include <twist/ed/wait/sys.hpp>
-#include "mutex.hpp"
 
 #include <cstdint>
 
@@ -17,41 +13,26 @@ class CondVar {
   // https://en.cppreference.com/w/cpp/named_req/BasicLockable
   template <class Mutex>
   void Wait(Mutex& mutex) {
-    twist::ed::stdlike::atomic<uint32_t> locked{1};
-    {
-      std::lock_guard queue_guard(queue_mutex_);
-      waiters_.emplace(&locked);
-    }
+    auto old_ticket = ticket_.load();
     mutex.unlock();
-    twist::ed::Wait(locked, 1);
+    twist::ed::Wait(ticket_, old_ticket);
     mutex.lock();
   }
 
   void NotifyOne() {
-    std::lock_guard guard(queue_mutex_);
-    if (!waiters_.empty()) {
-      NotifyOneImpl();
-    }
-  }
-
-  void NotifyAll() {
-    std::lock_guard guard(queue_mutex_);
-    while (!waiters_.empty()) {
-      NotifyOneImpl();
-    }
-  }
-
- private:
-  void NotifyOneImpl() {
-    auto& ind = *waiters_.front();
-    waiters_.pop();
-    auto wake_key = twist::ed::PrepareWake(ind);
-    ind.store(0);
+    auto wake_key = twist::ed::PrepareWake(ticket_);
+    ticket_++;
     twist::ed::WakeOne(wake_key);
   }
 
-  std::queue<twist::ed::stdlike::atomic<uint32_t>*> waiters_;
-  Mutex queue_mutex_;
+  void NotifyAll() {
+    auto wake_key = twist::ed::PrepareWake(ticket_);
+    ticket_++;
+    twist::ed::WakeAll(wake_key);
+  }
+
+ private:
+  twist::ed::stdlike::atomic<uint32_t> ticket_{0};
 };
 
 }  // namespace stdlike
